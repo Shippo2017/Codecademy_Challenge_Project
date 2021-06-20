8ca02e995ce6cde16617adc7e65308fcf4bbd00f
Shippo2017 <77732207+Shippo2017@users.noreply.github.com>
2021-06-20 09:30:19 +0700

    Create script.py

---
+++ Python/Learn_Python_Fundamental/Medical-Insurance-Project-Syntax/script.py
@@ -0,0 +1,61 @@
+# create the initial variables below
+age = 28
+sex = 0
+bmi = 26.2
+num_of_children = 3
+smoker = 0
+
+# Add insurance estimate formula below
+insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 2400 * smoker - 12500
+
+print("This person's insurance cost is" + " " + str(insurance_cost) + " " + "dollars.")
+
+# Age Factor
+age += 4
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 2400 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in cost of insurance after increasing the age by 4 years is" + " " + str(change_in_insurance_cost) + " " + "dollars.")
+
+# BMI Factor
+age = 28
+bmi += 3.1
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 2400 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated insurance cost after increasing BMI by 3.1 is" + " " + str(change_in_insurance_cost) + " " + "dollars.")
+
+# Male vs. Female Factor
+bmi = 26.2
+sex = 1
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 2400 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated cost for being male instead of female is" + " " + str(change_in_insurance_cost) + " " + "dollars.")
+
+# Extra Practice
+# children factor
+sex = 0
+num_of_children += 2
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 2400 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated cost insurance cost when having increasing num_of_children by 2 is" + " " + str(change_in_insurance_cost) + " " + "dollars.")
+
+# smoke factor
+num_of_children = 3
+smoker = 1
+
+new_insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 2400 * smoker - 12500
+
+change_in_insurance_cost = new_insurance_cost - insurance_cost
+
+print("The change in estimated cost for smoking female is" + " " + str(change_in_insurance_cost) + " " + "dollars.")
