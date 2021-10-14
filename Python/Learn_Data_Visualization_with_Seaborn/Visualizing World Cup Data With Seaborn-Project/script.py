19f9470c1af5e429604b8d9a21ac666b03a8a371
Shippo2017 <77732207+Shippo2017@users.noreply.github.com>
2021-10-14 09:00:32 +0700

    Update script.py

--- Python/Learn_Data_Visualization_with_Seaborn/Visualizing World Cup Data With Seaborn-Project/script.py
+++ Python/Learn_Data_Visualization_with_Seaborn/Visualizing World Cup Data With Seaborn-Project/script.py
@@ -6,18 +6,25 @@ import seaborn as sns
 df = pd.read_csv('WorldCupMatches.csv')
 df_goals = pd.read_csv('goals.csv')
 
+# No.5
 df["Total Goals"]= df['Home Team Goals'] + df['Away Team Goals']
-# print(df_goals.head())
 
+# No.6-11
 sns.set_style('whitegrid')
-sns.set_context('notebook', font_scale=1)
-
-f, ax = plt.subplots(figsize=(12,7))
+sns.set_context('poster', font_scale=0.8)
+f, ax = plt.subplots(figsize=(15,5))
 ax = sns.barplot(data=df, x='Year', y='Total Goals')
 ax.set_title('World Cup Data With Seaborn')
+plt.show()
+plt.clf()
 
-f, ax2 = plt.subplots(figsize=(12,7))
+# No.12
+print(df_goals.head())
+
+# No.13
+sns.set_style('whitegrid')
+sns.set_context('notebook', font_scale=1.25)
+f, ax2 = plt.subplots(figsize=(15,5))
 ax2 = sns.boxplot(data=df_goals, x='year', y='goals', palette='Spectral')
 ax2.set_title('Goals Visualization')
-
 plt.show()
