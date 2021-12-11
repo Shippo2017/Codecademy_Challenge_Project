6c6944466b0451d92b4155d4655ce75573b47996
Shippo2017 <77732207+Shippo2017@users.noreply.github.com>
2021-12-11 22:00:15 +0700

    Create players.py

---
+++ Python/Learn_Foundation_of_Machine_Learning/Sports Vector Machine-Project/players.py
@@ -0,0 +1,7 @@
+import pickle
+
+aaron_judge = pickle.load( open( "aaron_judge.p", "rb" ) )
+jose_altuve = pickle.load( open( "jose_altuve.p", "rb" ) )
+david_ortiz = pickle.load( open( "david_ortiz.p", "rb" ) )
+
+# data were from .p extention, don't know how to make it yet!
