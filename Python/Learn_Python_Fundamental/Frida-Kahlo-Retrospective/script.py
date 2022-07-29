4da6fe08aa59435031b503c4e358c644bf749d61
Shippo2017 <77732207+Shippo2017@users.noreply.github.com>
2022-07-29 19:11:01 +0700

    Create script.py

---
+++ Python/Learn_Python_Fundamental/Frida-Kahlo-Retrospective/script.py
@@ -0,0 +1,13 @@
+paintings = ['The Two Fridas', 'My Dress Hangs Here', 'Tree of Hope', 'Self Potrait With Monkeys']
+dates = [1939, 1933, 1946, 1940]
+
+paintings = list(zip(paintings, dates))
+print(paintings)
+
+paintings.append(('The Broken Column', 1944))
+paintings.append(('The Wounded Deer', 1946))
+paintings.append(('Me and My Doll', 1937))
+print(paintings)
+
+paintings_length = len(paintings)
+print(paintings_length)
